b0b18a1a26b2e3e663a10bf6ffe13e82542e474e
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-10 22:20:40 -0300

    Feat: Create Content

--- src/container/Content.jsx
+++ src/container/Content.jsx
@@ -1,7 +1,43 @@
-const Content = () => {
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import {
+  Navbar,
+  Feed,
+  ProfileDetail,
+  EditProfile,
+  Search,
+} from '../components';
+
+const Content = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>
-      <h1>Content</h1>
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-400">
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/profile-detail/:profileId"
+            element={<ProfileDetail user={user} />}
+          />
+          <Route path="/edit-profile" element={<EditProfile user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
 };
